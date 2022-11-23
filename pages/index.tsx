8c74a70eb42df258af8b3498039e506f7e39184c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2022-11-23 21:47:54 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [sentence, setSentence] = useState('Sebelah sini, bung!');
+  const initialSentences = [
+    'ini tidak seperti yang kamu bayangkan',
+    'disebelah sini, bung!',
+  ];
+  const initialSentence =
+    initialSentences[Math.floor(Math.random() * initialSentences.length)];
+  const [sentence, setSentence] = useState(initialSentence);
   const [oldSentence, setOldSentence] = useState('');
 
   const _convertSentence = () => {
     const newSentence = sentence
+      .replace(/w*i\b/gi, 'ie')
       .replace(/j/gi, 'dj')
       .replace(/u/gi, 'oe')
       .replace(/y/gi, 'j');
@@ -15,12 +23,17 @@ export default function Home() {
   // watch variable "sentence" change
   useEffect(() => {
     _convertSentence();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sentence]);
 
   return (
     <div className='max-w-xl mx-auto p-4'>
-      <h1 className='text-4xl font-bold'>Ubah kalimat biasa menjadi ejaan lama</h1>
+      <Head>
+        <title>Ejaan Lama</title>
+      </Head>
+      <h1 className='text-4xl font-bold text-green-500'>
+        Ubah kalimat biasa menjadi ejaan lama
+      </h1>
       <div>
         <textarea
           className='w-full mt-4 h-64 p-2 border border-gray-300 rounded'
@@ -30,7 +43,7 @@ export default function Home() {
         />
         <textarea
           className='w-full mt-4 h-64 p-2 border border-gray-300 rounded'
-          placeholder='Ini hasilnja boeng'
+          placeholder='Disini hasilnja boeng'
           value={oldSentence}
           readOnly
         />
